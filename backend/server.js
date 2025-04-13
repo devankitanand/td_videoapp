1bdfc61ad1604dcdf8afd5d994501c2e4033277d
Ankit Anand <devankitanand@gmail.com>
2025-04-14 01:10:27 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const progressRoutes = require('./routes/progressRoutes');
+import express, { json } from 'express';
+import { connect } from 'mongoose';
+import cors from 'cors';
+import progressRoutes from './routes/progressRoutes';
 require('dotenv').config();
 
 const app = express();
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 // Routes
 app.use('/api/progress', progressRoutes);
@@ -17,8 +17,7 @@ app.use('/api/progress', progressRoutes);
 // MongoDB Connection
 const MONGO_URI = process.env.MONGO_URI
 
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
