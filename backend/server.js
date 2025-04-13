c4045f14e0abf1284163b81a9cb96c9573858cb0
Ankit Anand <devankitanand@gmail.com>
2025-04-14 01:04:00 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const progressRoutes = require('./routes/progressRoutes');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
